fix major optimizer bug by adding self-defined gradient function to bypass autograd

src/main.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import autograd.numpy as np
+#import numpy as np
 import pandas as pd
 import random
 from matplotlib import pyplot as plt, dates
@@ -8,6 +9,7 @@ import time
 
 import utils
 import optimization
+import opt_new
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -20,8 +22,8 @@ x = np.linspace(0,2*np.pi, 100)
 x = np.sin(x)
 # x[30:40] = np.sin(np.linspace(0,4*np.pi, 10))
 # x[90:100] = np.sin(np.linspace(0,4*np.pi, 10))
-# signal = (x-np.mean(x))/np.std(x)
-signal = x
+signal = (x-np.mean(x))/np.std(x)
+# signal = x
 # signal = np.linspace(-1, 1, 5)
 # x = np.zeros(50)
 # x[10:30] = np.arange(20)
@@ -34,7 +36,7 @@ signal = x
 # x = np.linspace(-1, 1, 50)
 # signal = pd.Series((x-np.mean(x))/np.std(x))
 
-num_copies = 500
+num_copies = 5000
 sigma = 0.1
 max_shift= 0.1
 
@@ -43,14 +45,14 @@ start = time.time()
 observations, shifts = utils.generate_data(signal, num_copies,  max_shift, sigma, cyclic = True)
 print('time to generate data = ', time.time() - start)
 
-N, M = observations.shape
-fig, axes = plt.subplots(10,5, figsize=(20,20));
-ax = axes.flatten()
-for i in range(50):
-    lag = shifts[i]
-    ax[i].vlines(lag, np.min(observations[:,i]), np.max(observations[:,i]), color = 'red', ls = '-.')
-    ax[i].plot(observations[:,i])
-plt.savefig('observations')
+# N, M = observations.shape
+# fig, axes = plt.subplots(10,5, figsize=(20,20));
+# ax = axes.flatten()
+# for i in range(50):
+#     lag = shifts[i]
+#     ax[i].vlines(lag, np.min(observations[:,i]), np.max(observations[:,i]), color = 'red', ls = '-.')
+#     ax[i].plot(observations[:,i])
+# plt.savefig('observations')
  
 with open('data.npy', 'wb') as f:
     np.save(f, observations)
@@ -65,11 +67,11 @@ with open('data.npy', 'wb') as f:
 # mean_est, P_est, B_est = utils.invariants_from_data(observations)
 
 L = len(signal)
-# np.random.seed(42)
+np.random.seed(42)
 X0 = np.random.normal(0, 1, L)
-# X0 = signal.copy()
-# X0 += np.random.normal(0, 0.1, L)
-X_est = optimization.optimise_manopt(observations, sigma, X0, extra_inits=0)
+# X0 = np.zeros(L)
+X_est = optimization.optimise_manopt(observations, sigma, X0, extra_inits=2)
+# X_est = opt_new.optimise_manopt(observations, sigma, X0, extra_inits=0)
 
 with open('test.npy', 'wb') as f:
     np.save(f, X_est)
@@ -78,6 +80,6 @@ with open('test.npy', 'wb') as f:
 
 # print(X_est)
 # print(signal)
-print('error = ', np.linalg.norm(X_est-signal)/L)
+print('relative error = ', np.linalg.norm(X_est-signal)/np.linalg.norm(signal))
 
 

src/optimization.py
@@ -6,74 +6,21 @@ import pymanopt.optimizers
 
 import utils
 
-def make_A(L):
-    A = np.eye(L, dtype = 'complex_') 
-    A[0,:] += np.ones(L)
-    A[:,0] += np.ones(L)
-    return A
-
-def create_cost_function(mean_est, P_est, B_est, sigma, manifold):
-    euclidean_gradient = euclidean_hessian = None
-    
-    @pymanopt.function.autograd(manifold)
-    def cost(X):
-        L = len(X)
-        # print(type(X))
-        FX = np.fft.fft(X)
-        A = make_A(L)
-        
-        M1 = np.mean(X)
-        M2 = abs(FX)**2 + L * sigma**2 * np.ones(L)
-        # x = np.array(X)[:,i]
-        mat1 = np.array([np.roll(FX,k) for k in range(L)])
-        mat2 = np.outer(FX, np.conjugate(FX))
-        matmul = mat1 * mat2
-        M3 = matmul + mean_est * (sigma**2) * (L**2) * A
-            
-        # M3 = utils.bispectrum(X.reshape(L,1))[0] + mean_est * (sigma**2) * (L**2) * A
-    
-        M3_min_Best = M3 - B_est
-        
-        # compute coefficients
-        a1 = L**2
-        a2 = 1/(L*(2+sigma**2) )
-        a3 = 1/(L**2*(3+sigma**4))
-        
-        scale = 3 + sigma**4
-        assert M2.shape == P_est.shape
-        f = scale * 0.5 * \
-            (a1*(M1-mean_est)**2 + \
-                a2*np.linalg.norm(M2 - P_est,2)**2 + \
-                    a3*np.linalg.norm(M3_min_Best, 'fro')**2 
-            )
-                
-        return f
-    return cost, euclidean_gradient, euclidean_hessian
-    
-    
-    
-# matrix = anp.random.normal(size=(dim, dim))
-# matrix = 0.5 * (matrix + matrix.T)
-
-# @pymanopt.function.autograd(manifold)
-# def cost(point):
-#     return -point @ matrix @ point
-    
-    
-    
 def optimise_manopt(data, sigma, X0, extra_inits = 0):
     assert isinstance(extra_inits, int)
     L, N = data.shape
     mean_est, P_est, B_est = utils.invariants_from_data(data)
     
-    manifold = pymanopt.manifolds.Euclidean(L)
-    cost, euclidean_gradient, euclidean_hessian = create_cost_function(mean_est, P_est, B_est, sigma, manifold)
-    # print('EG: ', euclidean_gradient, ' EH: ', euclidean_hessian)
-    # print(type(cost))
-    problem = pymanopt.Problem(manifold, cost)
-    optimizer = pymanopt.optimizers.TrustRegions(min_gradient_norm = 1e-6, max_iterations = 200)
+    # manifold = pymanopt.manifolds.Euclidean(L,1)
+    manifold = pymanopt.manifolds.Euclidean(L,1)
+    cost, grad, euclidean_hessian = create_cost_function(mean_est, P_est, B_est, sigma, manifold)
+    # problem = pymanopt.Problem(manifold, cost, euclidean_gradient = grad, euclidean_hessian=euclidean_hessian)
+    problem = pymanopt.Problem(manifold, cost, riemannian_gradient=grad)
+    optimizer = pymanopt.optimizers.TrustRegions(min_gradient_norm = 1e-7, max_iterations = 200, verbosity = 1)
+    # optimizer = pymanopt.optimizers.NelderMead()
+    if X0.ndim == 1:
+        X0 = X0.reshape(-1,1)
     result = optimizer.run(problem, initial_point=X0)
-    # optimizer = pymanopt.optimizers.SteepestDescent()
     # result = optimizer.run(problem)
     X_est = result.point
     result_cost = result.cost
@@ -86,3 +33,157 @@ def optimise_manopt(data, sigma, X0, extra_inits = 0):
                 X_est = result.point
             
     return X_est
+
+
+
+def create_cost_function(mean_est, P_est, B_est, sigma, manifold):
+    euclidean_gradient = euclidean_hessian = None
+    
+    # @pymanopt.function.autograd(manifold)
+    # def cost(X):
+    #     L = len(X)
+    #     # print(type(X))
+    #     FX = np.fft.fft(X)
+    #     A = make_A(L)
+        
+    #     M1 = np.mean(X)
+    #     M2 = abs(FX)**2 + L * sigma**2 * np.ones(L)
+    #     # x = np.array(X)[:,i]
+    #     mat1 = np.array([np.roll(FX,k) for k in range(L)])
+    #     mat2 = np.outer(FX, np.conjugate(FX))
+    #     matmul = mat1 * mat2
+    #     M3 = matmul + mean_est * (sigma**2) * (L**2) * A
+            
+    #     # M3 = utils.bispectrum(X.reshape(L,1))[0] + mean_est * (sigma**2) * (L**2) * A
+    
+    #     M3_min_Best = M3 - B_est
+        
+    #     # compute coefficients
+    #     a1 = L**2
+    #     a2 = 1/(L*(2+sigma**2) )
+    #     a3 = 1/(L**2*(3+sigma**4))
+        
+    #     scale = 3 + sigma**4
+    #     assert M2.shape == P_est.shape
+    #     f = scale * 0.5 * \
+    #         (a1*(M1-mean_est)**2 + \
+    #             a2*np.linalg.norm(M2 - P_est,2)**2 + \
+    #                 a3*np.linalg.norm(M3_min_Best, 'fro')**2 
+    #         )
+                
+    #     return f
+    
+    @pymanopt.function.autograd(manifold)
+    def cost(X):
+        if X.ndim == 1:
+            X = X.reshape(-1,1)
+        L, K = X.shape
+        assert K == 1
+        # print(type(X))
+        FX = np.fft.fft(X, axis = 0)
+        A = make_A(L)
+        
+        # limits
+        M1 = np.mean(X)
+        M2 = abs(FX)**2 + L * sigma**2 * np.ones((L,K))
+        M3 = mean_est * (sigma**2) * (L**2) * A
+        # matmul = np.zeros((L,L,K))
+        for k in range(K):
+            y =FX[:,k]
+            mat1 = utils.circulant(y)
+            mat2 = np.outer(y, np.conjugate(y))
+            M3 += mat1 * mat2
+            
+        M3_min_Best = M3 - B_est
+        
+        # compute coefficients
+        a1 = L**2
+        a2 = 1/(L*(2+sigma**2) )
+        a3 = 1/(L**2*(3+sigma**4))
+        
+        scale = 3 + sigma**4
+        # assert M2.shape == P_est.shape
+        f = scale * 0.5 * \
+            (a1*(M1-mean_est)**2 + \
+                a2*np.linalg.norm(M2 - P_est.reshape(-1,1))**2 + \
+                    a3*np.linalg.norm(M3_min_Best, 'fro')**2 
+            )
+                
+        return f
+    
+    
+    @pymanopt.function.autograd(manifold)
+    def grad(X):
+        if X.ndim == 1:
+            X = X.reshape(-1,1)
+        L, K = X.shape
+        assert K == 1
+        # print(type(X))
+        FX = np.fft.fft(X, axis = 0)
+        A = make_A(L)
+        
+        # limits
+        M1 = np.mean(X)
+        M2 = abs(FX)**2 + L * sigma**2 * np.ones((L,K))
+        M3 = mean_est * (sigma**2) * (L**2) * A
+        for k in range(K):
+            y =FX[:,k]
+            mat1 = utils.circulant(y)
+            mat2 = np.outer(y, np.conjugate(y)) 
+            M3 += mat1 * mat2
+            
+        M3_min_Best = M3 - B_est
+        
+        # compute coefficients
+        a1 = L**2
+        a2 = 1/(L*(2+sigma**2) )
+        a3 = 1/(L**2*(3+sigma**4))
+        gradX = (a1/L) * (M1 - mean_est) * np.ones((L,K)) + \
+            2 * L * a2 * np.fft.ifft((M2-P_est.reshape(-1,1))*FX, axis = 0) 
+        for k in range(K):
+            gradX[:,k] += a3 * DBx_adj(FX, M3_min_Best)
+        
+        scale = 3 + sigma**4
+        gradX = scale * gradX.real
+        gradX = manifold.euclidean_to_riemannian_gradient(X, gradX)
+        # print('used defined grad')
+        return gradX
+    
+    return cost, grad, euclidean_hessian
+    # @pymanopt.function.numpy(manifold)
+    # def cost(X):
+    #     return -np.trace(X.T @ B_est @ X)
+
+    # @pymanopt.function.numpy(manifold)
+    # def euclidean_gradient(X):
+    #     return -2 * B_est @ X
+
+    # @pymanopt.function.numpy(manifold)
+    # def euclidean_hessian(X, H):
+    #     return -2 * B_est @ H
+    
+
+    #return cost, grad, euclidean_hessian
+    
+
+def make_A(L):
+    A = np.eye(L, dtype = 'complex_') 
+    A[0,:] += np.ones(L)
+    A[:,0] += np.ones(L)
+    return A
+    
+    
+# matrix = anp.random.normal(size=(dim, dim))
+# matrix = 0.5 * (matrix + matrix.T)
+
+# @pymanopt.function.autograd(manifold)
+# def cost(point):
+#     return -point @ matrix @ point
+    
+def DBx_adj(y, W):
+    y = y.reshape(-1,1)
+    L = y.shape[0]   
+    H = W * utils.circulant(y.conj())
+    z = L * np.fft.ifft(utils.circulantadj(W * (y @ y.conj().T).conj()) + (H + H.conj().T) @ y.reshape(-1,1),axis=0)
+    return z.flatten()
+

src/visualization.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import statsmodels.tsa.stattools as smt
+import os
 import utils
-# x = np.array([2, 3, 5, 7, 11, 13])
-# y = np.array([np.roll(x,k) for k in range(len(x))])
-# print(y)
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 x = np.arange(5)
 y = np.roll(x,1)
 x_aligned = utils.align_to_ref(x,y)
@@ -14,7 +16,7 @@ with open('test.npy', 'rb') as f:
     signal = np.load(f)
     X0 = np.load(f)
 
-
+X_est = X_est.flatten()
 plt.rcParams['text.usetex'] = True
 # use convolution theorem https://en.wikipedia.org/wiki/Cross-correlation
 signal_fft = np.fft.fft(signal)